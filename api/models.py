0cbfd99c726a4c69f16769c7359539b34529621c
sanya-kenneth <sanyakenneth@gmail.com>
2018-11-10 10:41:24 +0300

    Add user models and Validation

--- api/models.py
+++ api/models.py
@@ -1,42 +1,24 @@
 import datetime
+import re
 
 
 class Order:
-    """Class will contain all my parcel order objects"""
-    Order = [
-        {
-            'destination': 'Mukono',
-            'date': '23-11-2018',
-            'Pickup_location': 'Nakawa',
-            'price': 100000UGX,
-            'weight': 100kg,
-            '_id': 1111,
-        }
-    ]
+    """Class will contain all my parcel orders"""
 
     def __init__(self, *args):
         self.destination = args[0]
         self.price = args[1]
         self.weihgt = args[2]
-        self._id = args[3]
-        self.Pickup_location = args[4]
-        self.id = args[5]
+        self.Pickup_location = args[3]
+        self.id = args[4]
 
-    def Valid_order(self):
-        """
-        Method validates the attributes of an order.
-        : it should return:
-        True - if the given order details are all valid.
-        False - if one or all of the given details  are invalid.
-        """
-        if not self.destination or not self.price or not self.weihgt or\
-                self.destination.isspace():
-            return False
-        else:
-            return True
+    
 
-        if not self.Pickup_location or not self.id or not self.weihgt.isspace() or\
-                self.destination.isspace():
-            return False
-        else:
-            return True    
+class User:
+    """Class handles users of the SendIT app"""
+    users = []
+    def __init__(self, username, email, password, admin='false'):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.admin = admin
