fe85823eedf8f2af9e6099051f4bc31cf9459953
bekeplar <bekeplar@gmail.com>
2018-11-15 07:46:09 -0800

    Add get user route[Delivers-#161982900]

--- tests/test_order.py
+++ tests/test_order.py
@@ -83,6 +83,14 @@ class TestOrder(unittest.TestCase):
         reply = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
 
+    def test_fetch_specific_order_with_invalid_id(self):
+        """Test that a user can get an order which does  exist"""
+        response = self.client.get(
+        '/api/v1/orders/22'   
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 404)    
+
 
     def test_fetch_specific_order_from_empty_list(self):
         """Test that a user cannot get an order from an empty list"""
@@ -90,4 +98,241 @@ class TestOrder(unittest.TestCase):
             '/api/v1/orders/1'
         )
         reply = json.loads(response.data.decode())
-        self.assertEqual(response.status_code, 200)        
+        self.assertEqual(response.status_code, 200)
+
+    def test_fetch_specific_non_existing_order(self):
+        """
+        Test that a user cannot get 
+        an order that does not exist.
+        """
+        response = self.client.get(
+            '/api/v1/orders/1'
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)    
+
+    def test_add_order_with_missing_inputs(self):
+        """Testing whether missing input fields are not allowed"""
+        order = dict(
+            destination='',
+            date='',
+            Pickup_location='',
+            price=80000,
+            weight=75,
+            name='Bekalaze',
+            id=1
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 201)
+
+
+    def test_add_order_with_missing_name(self):
+        """
+        Testing whether missing 
+        input fields are not allowed
+        """
+        order = dict(
+        destination='Mukono',
+        date='23-11-2018',
+        Pickup_location='Nakawa',
+        price=80000,
+        weight=75,
+        name= '',
+        id=1
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertIn(
+            'Please fill all input fields!',
+            reply['message'])
+        
+
+    def test_add_order_with_missing_destination(self):
+        """
+        Testing whether missing 
+        input fields are not allowed
+        """
+        order = dict(
+        destination='',
+        date='23-11-2018',
+        Pickup_location='Nakawa',
+        price=80000,
+        weight=75,
+        name= 'Bekalaze',
+        id=1
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 201)
+        
+
+    def test_add_order_with_missing_pickup_location(self):
+        """
+        Testing whether missing 
+        input fields are not allowed
+        """
+        order = dict(
+        destination='Mukono',
+        date='23-11-2018',
+        Pickup_location='',
+        price=80000,
+        weight=75,
+        name= 'Bekalaze',
+        id=1
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 201)
+            
+ 
+    def test_add_order_with_missing_price(self):
+        """
+        Testing whether missing 
+        input fields are not allowed
+        """
+        order = dict(
+        destination='Mukono',
+        date='23-11-2018',
+        Pickup_location='Nakawa',
+        price='',
+        weight=75,
+        name= 'Bekalaze',
+        id=1
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 400)
+           
+    def test_create_order(self):
+        """Test the method to add an order"""
+        order = dict(
+        destination='Mukono',
+        date='23-11-2018',
+        Pickup_location='Nakawa',
+        price=80000,
+        weight=75,
+        name='Bekalaze',
+        id=1
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertIn(
+            'Order created successfully!',
+            reply['message'
+            ])
+          
+    def test_add_order_with_missing_id(self):
+        """
+        Testing whether missing 
+        input fields are not allowed
+        """
+        order = dict(
+        destination='Mukono',
+        date='23-11-2018',
+        Pickup_location='Nakawa',
+        price=80000,
+        weight='75',
+        name= 'Bekalaze',
+        id=''
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_create_existing_order(self):
+        """Test the method to add an order"""
+        order = dict(
+        destination='Mukono',
+        date='23-11-2018',
+        Pickup_location='Nakawa',
+        price=80000,
+        weight=75,
+        name='Bekalaze',
+        id=1
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertIn(
+            'Order created successfully!',
+            reply['message'
+            ])
+
+    def test_add_order_with_missing_weight(self):
+        """
+        Testing whether missing 
+        input fields are not allowed
+        """
+        order = dict(
+        destination='Mukono',
+        date='23-11-2018',
+        Pickup_location='Nakawa',
+        price=80000,
+        weight='',
+        name= 'Bekalaze',
+        id=1
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_order_with_missing_pickup_location(self):
+        """
+        Testing whether missing 
+        input fields are not allowed
+        """
+        order = dict(
+        destination='Mukono',
+        date='23-11-2018',
+        Pickup_location='',
+        price=80000,
+        weight=75,
+        name= 'Bekalaze',
+        id=1
+        )
+        response = self.client.post(
+            '/api/v1/orders',
+            content_type='application/json',
+            data=json.dumps(order)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 201)              
+               
+
--- tests/test_user.py
+++ tests/test_user.py
@@ -8,34 +8,74 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client(self)
 
-def test_signup():
-    """Test the method to signup a user"""
-    user = dict(
-    name='Bekalaze',
-    email= 'bekeplar@gmail.com',
-    password= 'Beka@43'
-    )
-    response = self.client.post(
-        '/api/v1/users',
-        content_type='application/json',
-        data=json.dumps(user)
-    )
-    reply = json.loads(response.data.decode())
-    self.assertIn('Bekalaze has been registered succesfully.')       
+
+    def test_signup(self):
+        """Test the method to signup a user"""
+        user = dict(
+        name='Bekalaze',
+        email= 'bekeplar@gmail.com',
+        password= 'Beka@43'
+        )
+        response = self.client.post(
+            '/api/v1/users',
+            content_type='application/json',
+            data=json.dumps(user)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertIn('Please try again.',reply['message'])   
+
+    def test_login_with_missing_input(self):
+        """Test the method to signup a user"""
+        user = dict(
+        name='',
+        email= 'bekeplar@gmail.com',
+        password= 'Beka@43'
+        )
+        response = self.client.post(
+            '/api/v1/users',
+            content_type='application/json',
+            data=json.dumps(user)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 400)               
 
 
-def test_register_empty_password(self):
-    """Test the method to signup a user"""
-    user = dict(
-    name='Bekalaze',
-    email= 'bekeplar@gmail.com',
-    password= ''
-    )
-    response = self.client.post(
-        '/api/v1/users',
-        content_type='application/json',
-        data=json.dumps(user)
-    )
-    reply = json.loads(response.data.decode())
-    self.assertIn('Password must be at least 4 characters.')       
+    def test_register_empty_password(self):
+        """Test the method to signup a user"""
+        user = dict(
+        name='Bekalaze',
+        email= 'bekeplar@gmail.com',
+        password= ''
+        )
+        response = self.client.post(
+            '/api/v1/users',
+            content_type='application/json',
+            data=json.dumps(user)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 400)  
 
+    def test_fetch_a_non_existing_specific_user(self):
+        """Test that a non existing user can be obtained"""
+        response = self.client.get(
+        '/api/v1/orders/1122'   
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 404) 
+
+    def test_name_already_exists(self):
+        """Test an existing user"""
+        user = dict(
+        name='Bekalaze',
+        email= 'bekeplar@gmail.com',
+        password= 'Beka@43'
+        )
+        response = self.client.post(
+            '/api/v1/users',
+            content_type='application/json',
+            data=json.dumps(user)
+        )
+        reply = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 400)
+        
+           
